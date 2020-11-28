3bb4166d1df548d456e992e152445ad624ca2271
Daisuke Kurihara UME <d.kurihara@ume-group.co.jp>
2020-11-28 15:15:23 +0900

    complete testing chrome debugger

---
+++ main.js
@@ -0,0 +1,9 @@
+function showSum(num1, num2){
+    var sum = num1+num2;
+    console.log("the button is pressed and the value is computed: " + sum);
+    alert(sum);
+}
+function logEvent(event){
+    console.log(event);
+    alert("event is logged. Check the console.");
+}
